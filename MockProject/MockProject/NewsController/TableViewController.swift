dc23ac40c6ae1c95ee57ea46dc3ff94b7d4be70d
DuongTuan1137 <duongtuan1137@gmail.com>
2019-09-13 02:48:00 -0700

    Thực Hiện LoadMore

--- MockProject/MockProject/NewsController/TableViewController.swift
+++ MockProject/MockProject/NewsController/TableViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class TableViewController: UITableViewController {
     var arrayNews = [NewsStruct]()
+    var pageIndex = 1
+    var pageSize = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getApi()
         registerForCell()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
     }
+    
     private func getApi(){
-        guard let url = URL(string:"http://812f8957.ngrok.io/18175d1_mobile_100_fresher/public/api/v0/listNews") else {return}
+        let api = "http://812f8957.ngrok.io/18175d1_mobile_100_fresher/public/api/v0/listNews?pageIndex=\(pageIndex)&pageSize=\(pageSize)"
+        guard let url = URL(string: api) else {return}
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {return}
             do{
                 let json = try JSONDecoder().decode(JsonStruct.self, from: data)
                 DispatchQueue.main.async {
-                    self.arrayNews = json.response.news
+                    json.response.news.forEach({ (news) in
+                        self.arrayNews.append(news)
+                    })
                     self.tableView.reloadData()
                 }
             } catch{
@@ -30,86 +39,43 @@ class TableViewController: UITableViewController {
             }
         }.resume()
     }
-    
-
-
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-        
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return arrayNews.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath) as! NewsTableViewCell
         cell.setData(news: arrayNews[indexPath.row])
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 350
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == arrayNews.count - 1 {
+            loadMore()
+        }
+    }
+    
+    private func loadMore() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.pageIndex += 1
+            self.getApi()
+        }
+    }
+    
+    @objc func refreshData(){
+        arrayNews.removeLast(arrayNews.count - pageSize)
+        pageIndex = 1
+        tableView.reloadData()
+        refreshControl?.endRefreshing()
     }
     
     private func registerForCell(){
         self.tableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTableViewCell")
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
--- MockProject/MockProject/PopulerController/PopularTableViewController.swift
+++ MockProject/MockProject/PopulerController/PopularTableViewController.swift
@@ -10,21 +10,35 @@ import UIKit
 
 class PopularTableViewController: UITableViewController {
     var arrayEvents = [EventsStruct]()
+    var pageIndex = 1
+    var pageSize = 10
     override func viewDidLoad() {
         super.viewDidLoad()
         getApi()
         registerForCell()
-
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
     }
+    
+    @objc func refreshData(){
+        arrayEvents.removeLast(arrayEvents.count - pageSize)
+        pageIndex = 1
+        tableView.reloadData()
+        refreshControl?.endRefreshing()
+    }
+    
     private func getApi(){
-        guard let url = URL(string:"http://812f8957.ngrok.io/18175d1_mobile_100_fresher/public/api/v0/listPopularEvents") else {return}
+        let api = "http://812f8957.ngrok.io/18175d1_mobile_100_fresher/public/api/v0/listPopularEvents?pageIndex=\(pageIndex)&pageSize=\(pageSize)"
+        guard let url = URL(string:api) else {return}
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {return}
             if error == nil {
                 do{
                     let json = try JSONDecoder().decode(PopularStruct.self, from: data)
                     DispatchQueue.main.async {
-                        self.arrayEvents = json.response.events
+                        json.response.events.forEach({ (event) in
+                            self.arrayEvents.append(event)
+                        })
                         self.tableView.reloadData()
                     }
                 } catch let err {
@@ -34,21 +48,33 @@ class PopularTableViewController: UITableViewController {
         }.resume()
     }
 
-    // MARK: - Table view data source
+    private func loadMore(){
+        pageIndex += 1
+        getApi()
+    }
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return arrayEvents.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PopularTableViewCell", for: indexPath) as! PopularTableViewCell
         cell.setupData(events: arrayEvents[indexPath.row])
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 366
     }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == arrayEvents.count - 1{
+            loadMore()
+        }
+    }
+    
     private func registerForCell(){
         self.tableView.register(UINib(nibName: "PopularTableViewCell", bundle: nil), forCellReuseIdentifier: "PopularTableViewCell")
     }
